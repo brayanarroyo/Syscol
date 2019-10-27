e0463db89b04515cd76742d1e7f7735f6dcf7cce
Brayan <spa_brayan@hotmail.com>
2019-10-27 15:17:37 -0600

    Se agregaron los campos de fecha y hora con funcionalidades

--- monitoreo.js
+++ monitoreo.js
@@ -54,4 +54,25 @@ $(document).ready(function(){
 		return false;
 	});
 
+	//Funcionalidad de los campos de fecha
+	$('.ui.calendar').calendar({
+		type: 'date',
+		monthFirst: false,
+		formatter: {
+			date: function (date, settings) {
+				if (!date) return '';
+				var day = date.getDate();
+				var month = date.getMonth() + 1;
+				var year = date.getFullYear();
+				return day + '/' + month + '/' + year;
+			}
+		}
+	});
+
+	//Funcionalidad de los campos de fecha
+	$('.ui.calendar.time').calendar({
+		ampm: false,
+		type: 'time'
+	});
+
 });
